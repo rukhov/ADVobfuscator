599a4d5302ba53db8199e72074fcc5c031be8746
Roman Ukhov <ukhov.roman@gmail.com>
2025-10-20 14:32:50 +0300

    Squashed commit of the following:
    commit 2cf6516b9fee7bcf3437e80fa4c01a12f5af0a25
    Author: Roman Ukhov <ukhov.roman@gmail.com>
    Date:   Mon Oct 20 14:32:39 2025 +0300
    
        fix

--- include/advobfuscator/aes_string.h
+++ include/advobfuscator/aes_string.h
@@ -28,28 +28,30 @@
 #ifndef ADVOBFUSCATOR_AES_STRING_H
 #define ADVOBFUSCATOR_AES_STRING_H
 
-#include <cstddef>
-#include <algorithm>
 #include "aes.h"
 #include "call.h"
+#include <algorithm>
+#include <cstddef>
+
 
 namespace andrivet::advobfuscator {
 
-  /// A compile-time string encrypted with AES-CTR.
-  template<std::size_t N>
-  struct AesString {
+/// A compile-time string encrypted with AES-CTR.
+template <std::size_t N>
+struct AesString {
     /// Construct a compile-time string encrypted with AES-CTR.
     /// \lparam str Array of characters to be encrypted at compile-time.
     /// \remark A key and a nonce are generated on the fly.
     consteval AesString(const char (&str)[N]) noexcept
-    : key_{generate_random_block<16>(generate_sum(str, 0))},
-      nonce_{generate_random_block<8>(generate_sum(str, 16))} {
-      // Compile-time copy of the data
-      std::copy(str, str + N, data_.begin());
-      // Compile-time encryption
-      auto encrypted = encrypt_ctr(data_, key_, nonce_);
-      // Compile-time copy of the encrypted data
-      std::copy(encrypted.begin(), encrypted.end(), data_.begin());
+        : key_{ generate_random_block<16>(generate_sum(str, 0)) },
+          nonce_{ generate_random_block<8>(generate_sum(str, 16)) }
+    {
+        // Compile-time copy of the data
+        std::copy(str, str + N, data_.begin());
+        // Compile-time encryption
+        auto encrypted = encrypt_ctr(data_, key_, nonce_);
+        // Compile-time copy of the encrypted data
+        std::copy(encrypted.begin(), encrypted.end(), data_.begin());
     }
 
     /// Destruct the string by first erasing its content.
@@ -57,26 +59,29 @@ namespace andrivet::advobfuscator {
     constexpr ~AesString() noexcept { erase(); }
 
     /// Implicit conversion to a pointer to (const) characters, like a regular string.
-    operator const char *() noexcept {
-      constexpr auto random = call::generate_random(__LINE__);
-      const ObfuscatedMethodCall call{random, &AesString::decrypt_inplace};
-      call(random, this);
-      return reinterpret_cast<const char *>(data_.data());
+    operator const char*() noexcept
+    {
+        constexpr auto random = call::generate_random(__LINE__);
+        const ObfuscatedMethodCall call{ random, &AesString::decrypt_inplace };
+        call(random, this);
+        return reinterpret_cast<const char*>(data_.data());
     }
 
     /// Descrypt the encrypted string.
-    [[nodiscard]] constexpr std::string decrypt() const {
-      std::array<std::uint8_t, N> buffer;
-      std::copy(data_.begin(), data_.end(), buffer.begin());
-      if(encrypted_) decrypt_ctr(buffer.begin(), N, key_, nonce_);
-      std::string str;
-      str.resize(N - 1);
-      std::copy(buffer.begin(), buffer.end() - 1, str.begin());
-      return str;
+    [[nodiscard]] constexpr std::string decrypt() const
+    {
+        std::array<std::uint8_t, N> buffer;
+        std::copy(data_.begin(), data_.end(), buffer.begin());
+        if (encrypted_)
+            decrypt_ctr(buffer.data(), N, key_, nonce_);
+        std::string str;
+        str.resize(N - 1);
+        std::copy(buffer.begin(), buffer.end() - 1, str.begin());
+        return str;
     }
 
     /// Get the raw (encrypted) content.
-    [[nodiscard]] const char *raw() const noexcept { return data_.data(); }
+    [[nodiscard]] const char* raw() const noexcept { return (const char*)data_.data(); }
 
     /// Get the actual length of the string.
     [[nodiscard]] constexpr std::size_t size() noexcept { return N - 1; }
@@ -90,26 +95,33 @@ namespace andrivet::advobfuscator {
     /// The key used to encrypt the data.
     Key key_{};
 
-  private:
+private:
     /// Erase the information stored by the string (data, key and nonce)
-    constexpr void erase() noexcept {
-      if (encrypted_) return;
-      std::fill(data_.begin(), data_.end(), 0);
-      std::fill(key_.begin(), key_.end(), 0);
-      std::fill(nonce_.begin(), nonce_.end(), 0);
+    constexpr void erase() noexcept
+    {
+        if (encrypted_)
+            return;
+        std::fill(data_.begin(), data_.end(), 0);
+        std::fill(key_.begin(), key_.end(), 0);
+        std::fill(nonce_.begin(), nonce_.end(), 0);
     }
 
     /// Run-time decryption
-    void decrypt_inplace() noexcept {
-      if(!encrypted_) return;
-      decrypt_ctr(reinterpret_cast<Byte*>(data_.data()), N, key_, nonce_);
-      encrypted_ = false;
+    void decrypt_inplace() noexcept
+    {
+        if (!encrypted_)
+            return;
+        decrypt_ctr(reinterpret_cast<Byte*>(data_.data()), N, key_, nonce_);
+        encrypted_ = false;
     }
-  };
+};
 
-  /// User-defined literal "_aes"
-  template<AesString str>
-  consteval auto operator""_aes() { return str; }
+/// User-defined literal "_aes"
+template <AesString str>
+consteval auto operator""_aes()
+{
+    return str;
 }
+} // namespace andrivet::advobfuscator
 
 #endif
